715aaf09f4d317f92dbc7568513c9191d2905f1f
Vanessa Tagliente <e227426@edu.sbl.ch>
2023-10-31 11:26:08 +0100

    A1, A2 fertig, A3 angefangen

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 export function aufgabe01(args) {
   const input = args
   const result = []
-
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+
+    if (currentElement === "e") {
+      //mache nichts
+      }
+    else if (currentElement === "E") {
+      //mache nichts
+    }
+    else {
+      result.push(currentElement)
+    }
+  
+  }
+  return result.join("")
+}
+
+export function aufgabe02 (args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+      result.push(currentElement.toUpperCase()) //toUpperCase --> alles gross
+        
+  }
+  return result.join("")
+}
+
+export function aufgabe03 (args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+    String.length.count(currentElement === "e")    
+    String.length.count(currentElement === "E")
+    
   }
   return result.join("")
 }
