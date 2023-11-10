58b152cca77d6645f5d32bc667e4a52bf77c4d68
Vanessa Tagliente <e227426@edu.sbl.ch>
2023-11-10 09:07:09 +0100

    an A3 gearbeit nicht geschafft, A4 gelöscht, A8 angefangen halb erledigt

--- script.js
+++ script.js
@@ -36,9 +36,49 @@ export function aufgabe03 (args) {
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
 
-    String.length.count(currentElement === "e")    
-    String.length.count(currentElement === "E")
+// irgendwie anders machen, Zähler hinzufügen?
+
+    if (currentElement === "e") {
+      String.length.count(currentElement === "e")
+      }
+    else if (currentElement === "E") {
+      String.length.count(currentElement === "E")
+    }
+    else {
+      result.push(currentElement)
+    }  
     
+  }
+  return result.join("")
+}
+
+export function aufgabe05 (args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+    result.push(currentElement.toLowerCase()) // toLowerCase --> alle Buchstaben klein
+    
+  }
+
+  return result.join("")
+}
+
+export function aufgabe08(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+    if (currentElement === "e") {
+      currentElement === "3" //ersetze 'e' mit '3'
+      }
+   
+    else {
+      result.push(currentElement)
+    }
+  
   }
   return result.join("")
 }
