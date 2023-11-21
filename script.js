981b1b4280055bb7d98b648dc9a717aec3a2ff34
Vanessa Tagliente <e227426@edu.sbl.ch>
2023-11-21 10:42:53 +0100

    A6 fertig

--- script.js
+++ script.js
@@ -82,6 +82,19 @@ export function aufgabe05 (args) {
   return false
 }
 
+export function aufgabe06 (args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    
+    if (currentElement === ".") {
+      return true
+    }
+  }
+  return false
+}
+
 export function aufgabe07 (args) {
   const input = args
   const result = []
